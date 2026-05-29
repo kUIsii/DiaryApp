661e6d881e3d9ca90c7a4147b9cd1bbb3f08c8fd
陈仕杰 <2453759261@qq.com>
2026-05-30 01:26:44 +0800

    chore: bump experimental version to 2.4.0-experimental

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ android {
         create("experimental") {
             dimension = "version"
             applicationId = "com.diary.app.experimental"
-            versionName = "2.3.0-experimental"
+            versionName = "2.4.0-experimental"
         }
     }
 
