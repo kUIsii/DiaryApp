46eb9d72c758f2eddf5aeb7cb6071189f17d53bb
陈仕杰 <2453759261@qq.com>
2026-05-30 12:46:57 +0800

    chore: bump experimental version to 2.6.0-experimental
    Changes:
    - Fix 10+ diary core bugs (P0/P1)
    - Simplify home UI with collapsible calendar
    - Add unified design language (DesignTokens)
    - Improve reading comfort in detail view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ android {
         create("experimental") {
             dimension = "version"
             applicationId = "com.diary.app.experimental"
-            versionName = "2.5.1-experimental"
+            versionName = "2.6.0-experimental"
         }
     }
 
