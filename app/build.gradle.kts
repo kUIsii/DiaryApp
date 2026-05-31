b9b6f5d7015c585c3bb2119b6d4e0b0a7f585a4b
陈仕杰 <2453759261@qq.com>
2026-05-31 20:37:02 +0800

    chore: bump version to 2.28.0-experimental (versionCode 21)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.diary.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 20
+        versionCode = 21
         versionName = "1.10.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -55,7 +55,7 @@ android {
         create("experimental") {
             dimension = "version"
             applicationId = "com.diary.app.experimental"
-            versionName = "2.27.1-experimental"
+            versionName = "2.28.0-experimental"
         }
     }
 
