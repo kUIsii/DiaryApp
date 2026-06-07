b9c86d1b65718a0967b9e69c808f2789e45f073b
陈仕杰 <2453759261@qq.com>
2026-06-08 01:27:32 +0800

    chore: bump experimental release to 2.61.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,8 +55,8 @@ android {
         create("experimental") {
             dimension = "version"
             applicationId = "com.diary.app.experimental"
-            versionCode = 53
-            versionName = "2.61.7-experimental"
+            versionCode = 54
+            versionName = "2.61.8-experimental"
         }
     }
 
