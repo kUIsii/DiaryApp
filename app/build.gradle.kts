eb93f947d754f6a9e5b95bddc2e9e2275bb8abd5
陈仕杰 <2453759261@qq.com>
2026-06-12 11:08:25 +0800

    chore: bump to 2.61.67

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,8 @@ android {
         create("experimental") {
             dimension = "version"
             applicationId = "com.diary.app.experimental"
-            versionCode = 66
-            versionName = "2.61.66-experimental"
+            versionCode = 67
+            versionName = "2.61.67-experimental"
         }
     }
 
