9e04741459d95e8de08c2e6705a6e5d3ff9a163a
陈仕杰 <2453759261@qq.com>
2026-06-15 22:41:42 +0800

    bump version to v2.64.05

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.diary.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 26404
-        versionName = "2.64.04"
+        versionCode = 26405
+        versionName = "2.64.05"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -64,7 +64,7 @@ android {
             dimension = "version"
             applicationId = "com.diary.app.experimental"
             versionCode = 101
-            versionName = "2.64.04-experimental"
+            versionName = "2.64.05-experimental"
         }
     }
 
