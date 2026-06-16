566078e33453b147ab4042835b99fe5390905498
陈仕杰 <2453759261@qq.com>
2026-06-16 22:45:38 +0800

    bump version to v2.64.20-experimental

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,8 @@ android {
         create("experimental") {
             dimension = "version"
             applicationId = "com.diary.app.experimental"
-            versionCode = 115
-            versionName = "2.64.19-experimental"
+            versionCode = 116
+            versionName = "2.64.20-experimental"
         }
     }
 
