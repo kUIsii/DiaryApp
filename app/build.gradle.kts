e6829a87412a9811f75667ffd818e5ff3f211aae
陈仕杰 <2453759261@qq.com>
2026-06-16 11:25:29 +0800

    bump version to v2.64.08

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.diary.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 26407
-        versionName = "2.64.07"
+        versionCode = 26408
+        versionName = "2.64.08"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -63,8 +63,8 @@ android {
         create("experimental") {
             dimension = "version"
             applicationId = "com.diary.app.experimental"
-            versionCode = 103
-            versionName = "2.64.07-experimental"
+            versionCode = 104
+            versionName = "2.64.08-experimental"
         }
     }
 
