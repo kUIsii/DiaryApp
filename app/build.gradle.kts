7f0569b8e049d2f1c9c967056893c2bf227e3968
陈仕杰 <2453759261@qq.com>
2026-06-17 23:51:14 +0800

    bump version to 2.64.47-experimental

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.diary.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 26427
-        versionName = "2.64.27-experimental"
+        versionCode = 26447
+        versionName = "2.64.47-experimental"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
