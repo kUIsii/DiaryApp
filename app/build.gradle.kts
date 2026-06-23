0843cbc390180e30c062d20ca0038d86c910378e
陈仕杰 <2453759261@qq.com>
2026-06-23 18:46:50 +0800

    chore: bump version to 2.64.90-experimental

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.diary.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 26489
-        versionName = "2.64.89-experimental"
+        versionCode = 26490
+        versionName = "2.64.90-experimental"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -66,8 +66,8 @@ android {
         create("experimental") {
             dimension = "version"
             applicationId = "com.diary.app.experimental"
-            versionCode = 26489
-            versionName = "2.64.89-experimental"
+            versionCode = 26490
+            versionName = "2.64.90-experimental"
         }
     }
 
