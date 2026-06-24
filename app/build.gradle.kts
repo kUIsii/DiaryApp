ad1d2fa7fe4bc5b292b8140fcf7cf88d6d50c18b
陈仕杰 <2453759261@qq.com>
2026-06-24 11:27:22 +0800

    release: v2.64.99-experimental

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.diary.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 26498
-        versionName = "2.64.98-experimental"
+        versionCode = 26499
+        versionName = "2.64.99-experimental"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -66,8 +66,8 @@ android {
         create("experimental") {
             dimension = "version"
             applicationId = "com.diary.app.experimental"
-            versionCode = 26498
-            versionName = "2.64.98-experimental"
+            versionCode = 26499
+            versionName = "2.64.99-experimental"
         }
     }
 
