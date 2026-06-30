81d83d07171308f6829df8274d3bc9416b42c516
陈仕杰 <2453759261@qq.com>
2026-06-30 12:57:17 +0800

    release: v2.78.01-experimental

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.diary.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 27800
-        versionName = "2.78.00-experimental"
+        versionCode = 27802
+        versionName = "2.78.01-experimental"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
